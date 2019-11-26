ff7032be914579e68ceaf39690a00962116137bb
Egor Matvienko <44952466+Ekispest@users.noreply.github.com>
2019-11-26 19:23:21 +0300

    Initial commit

---
+++ include/task2.h
@@ -0,0 +1,10 @@
+#include <iostream>
+
+template<typename T,size_t size>
+T* createArr(T(*gen)()){
+    auto a=new T[size];
+    for(size_t i=0;i<size;i++){
+        a[i]=gen();
+    }
+    return a;
+}
